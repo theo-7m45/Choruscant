bede4192fc784449c0049308526ea09d27a7a7af
theo-7m45 <theo.messean.pro@gmail.com>
2026-09-23 12:29:09 +0200

    Model et Migration Music et User

---
+++ Choruscant_Project/app/Models/Music.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use App\Models\User;
+
+
+class Music extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'title',
+        'youtube_video_id',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
---
+++ Choruscant_Project/database/migrations/2026_09_23_095653_create_music_table.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('musics', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->string('title');
+            $table->string('youtube_video_id');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('musics');
+    }
+};
