d3f9d78fe4939d7a5984b795235d5d35699176a9
theo-7m45 <theo.messean.pro@gmail.com>
2026-09-23 16:49:45 +0200

    Register.blade

--- Choruscant_Project/resources/views/auth/register.blade.php
+++ Choruscant_Project/resources/views/auth/register.blade.php
@@ -1,7 +1,28 @@
-<1>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Inscription</title>
+</head>
 
-<form action="..." method="...">
+<body>
+<h1>Inscription</h1>
+<form action="{{ route('register') }}" method="POST">
+    @csrf
+    <label for="name">Nom :</label>
+    <input type="text" name="name" id="name" required><br>
 
-    <!-- tes champs ici -->
+    <label for="email">Email :</label>
+    <input type="email" name="email" id="email" required><br>
 
+    <label for="password">Mot de passe :</label>
+    <input type="password" name="password" id="password" required><br>
+
+    <label for="password_confirmation">Confirmer le mot de passe :</label>
+    <input type="password" name="password_confirmation" id="password_confirmation" required><br>
+
+    <button type="submit">S'inscrire</button>
 </form>
+
+</body>
+</html>
